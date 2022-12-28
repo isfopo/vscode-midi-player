f517a642e4dac094adc8c6986addf07c1e084961
Isaac Poole <isaac@switcherstudio.com>
2022-12-28 14:13:19 -0500

    moves app routes to separate file

---
+++ src/webviews/src/AppRoutes.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useEffect } from 'react'
+import {
+  MemoryRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+  useNavigate,
+} from 'react-router-dom'
+import { useMidiData } from './hooks/useMidiData'
+import { MainView } from './views/MainView'
+import VSCodeAPI from './VSCodeAPI'
+
+const rootEl = document.getElementById('root')
+
+export const AppRoutes = () => {
+  const location = useLocation()
+  const navigate = useNavigate()
+  const midi = useMidiData()
+
+  useEffect(() => {
+    if (rootEl) {
+      navigate(`/${rootEl.dataset.route}`, { replace: true })
+    }
+  }, [])
+
+  return (
+    <Routes>
+      <Route path="view1" element={<MainView midi={midi} />} />
+    </Routes>
+  )
+}
--- src/webviews/src/index.tsx
+++ src/webviews/src/index.tsx
@@ -1,15 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import { MainView } from './views/MainView'
 import './vscode.css'
-import {
-  MemoryRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-  useNavigate,
-} from 'react-router-dom'
+import { MemoryRouter as Router } from 'react-router-dom'
 import VSCodeAPI from './VSCodeAPI'
+import { AppRoutes } from './AppRoutes'
 
 let workspace = ''
 
@@ -19,36 +13,7 @@ if (root) {
   workspace = root.getAttribute('data-workspace') || ''
 }
 
-window.addEventListener('message', e => {
-  // Here's where you'd do stuff with the message
-  // Maybe stick it into state management or something?
-  const message = e.data
-  console.debug(message)
-})
-
 const rootEl = document.getElementById('root')
-
-function AppRoutes() {
-  let location = useLocation()
-  let navigate = useNavigate()
-  useEffect(() => {
-    if (rootEl) {
-      navigate(`/${rootEl.dataset.route}`, { replace: true })
-    }
-  }, [])
-
-  useEffect(() => {
-    VSCodeAPI.onMessage(message => {
-      console.log(message.data)
-    })
-  }, [])
-
-  return (
-    <Routes>
-      <Route path="view1" element={<MainView />} />
-    </Routes>
-  )
-}
 
 ReactDOM.render(
   <React.StrictMode>
