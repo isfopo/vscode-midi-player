905c9cebbfe8321583e96e899728955cd76cc3f2
Isaac Poole <isaac@switcherstudio.com>
2022-12-28 14:12:46 -0500

    creates hooks for midi data management

---
+++ src/webviews/src/hooks/useMidiData.ts
@@ -0,0 +1,23 @@
+import { Midi } from '@tonejs/midi'
+import { useEffect } from 'react'
+import VSCodeAPI from '../VSCodeAPI'
+import useVSCodeState from './useVSCodeState'
+
+export const useMidiData = () => {
+  const [midi, setMidi] = useVSCodeState<Midi | undefined>(
+    undefined,
+    'midiData'
+  )
+
+  useEffect(() => {
+    VSCodeAPI.onMessage(message => {
+      setMidi(message.data)
+    })
+
+    VSCodeAPI.postMessage({
+      type: 'request',
+    })
+  }, [])
+
+  return midi
+}
