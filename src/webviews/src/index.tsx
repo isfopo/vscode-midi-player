e6449cd5dcb839abb497df524e7100f001d4d81d
Isaac Poole <isaac@switcherstudio.com>
2022-12-29 09:55:58 -0500

    rename root element var

--- src/webviews/src/index.tsx
+++ src/webviews/src/index.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 import { MemoryRouter as Router } from 'react-router-dom'
 import { AppRoutes } from './AppRoutes'
 
-const rootEl = document.getElementById('root')
+const root = document.getElementById('root')
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router initialEntries={[rootEl?.dataset.route ?? '']} initialIndex={0}>
+    <Router initialEntries={[root?.dataset.route ?? '']} initialIndex={0}>
       <AppRoutes />
     </Router>
   </React.StrictMode>,
-  rootEl
+  root
 )
