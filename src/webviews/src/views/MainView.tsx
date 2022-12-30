8d14a8b75527bd9e9e0cce79824a0ba3ceda30a0
Isaac Poole <isaac@switcherstudio.com>
2022-12-29 22:31:36 -0500

    plays back midi file

--- src/webviews/src/views/MainView.tsx
+++ src/webviews/src/views/MainView.tsx
@@ -1,13 +1,19 @@
 import { Midi } from '@tonejs/midi'
 import React from 'react'
+import { usePlayback } from '../hooks/usePlayback'
+import { VSCodeButton as Button } from '@vscode/webview-ui-toolkit/react'
 
 export interface MainViewProps {
   midi: Midi
 }
 
 export const MainView: React.FC<MainViewProps> = ({ midi }) => {
+  const { play, stop, isPlaying } = usePlayback(midi)
+
   return (
     <div>
+      <Button onClick={play}>Play</Button>
+      <Button onClick={stop}>Stop</Button>
       <h1>{midi.name}</h1>
       {midi.tracks.map(track => (
         <p>{track.instrument.name}</p>
