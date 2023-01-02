401a502e2e3f481aa19f1f69cf4770e18271a154
Isaac Poole <isaac@switcherstudio.com>
2023-01-02 01:20:33 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ const path = require('path')
  * @type {import('vite').UserConfig}
  */
 export default defineConfig({
-  root: 'src/webviews',
   publicDir: 'src/webviews/public',
   build: {
     outDir: 'out/webviews',
