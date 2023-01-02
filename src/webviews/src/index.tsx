ea992c5f53eed854b5ac70d73903ca6554923ca3
Isaac Poole <isaac@switcherstudio.com>
2023-01-02 01:52:47 -0500

    resizes icons

--- src/webviews/src/index.tsx
+++ src/webviews/src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { MemoryRouter as Router } from 'react-router-dom'
 import { AppRoutes } from './AppRoutes'
+import { IconContext } from 'react-icons'
 
 const root = document.getElementById('root')
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router initialEntries={[root?.dataset.route ?? '']} initialIndex={0}>
-      <AppRoutes />
-    </Router>
+    <IconContext.Provider value={{ size: '2rem' }}>
+      <Router initialEntries={[root?.dataset.route ?? '']} initialIndex={0}>
+        <AppRoutes />
+      </Router>
+    </IconContext.Provider>
   </React.StrictMode>,
   root
 )
