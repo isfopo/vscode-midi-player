5a6336e6c95423c72f9954cc2ae8b0ee9c4c9ceb
Isaac Poole <isaac@switcherstudio.com>
2023-01-05 01:21:55 -0500

    uses memos for x and width

--- src/webviews/src/components/Tracks/Note.tsx
+++ src/webviews/src/components/Tracks/Note.tsx
@@ -6,16 +6,17 @@ export interface NoteProps {
 }
 
 export const Note: React.FC<NoteProps> = ({ note }) => {
+  const x = useMemo<number>(() => {
+    return note.ticks / 1000
+  }, [note.midi])
+
   const y = useMemo<number>(() => {
     return note.midi / -10 + 12.7
   }, [note.midi])
 
-  return (
-    <rect
-      x={note.ticks / 1000}
-      y={y}
-      width={note.durationTicks / 1000}
-      height=".5"
-    />
-  )
+  const width = useMemo<number>(() => {
+    return note.durationTicks / 1000
+  }, [note.midi])
+
+  return <rect x={x} y={y} width={width} height=".5" />
 }
