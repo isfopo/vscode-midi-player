da3f9724c91832e7ab7469f698dafa659dd2757b
Isaac Poole <isaac@switcherstudio.com>
2023-01-05 01:09:11 -0500

    flips notes

--- src/webviews/src/components/Tracks/Note.tsx
+++ src/webviews/src/components/Tracks/Note.tsx
@@ -1,15 +1,19 @@
 import { Note as INote } from '@tonejs/midi/dist/Note'
-import React from 'react'
+import React, { useMemo } from 'react'
 
 export interface NoteProps {
   note: INote
 }
 
 export const Note: React.FC<NoteProps> = ({ note }) => {
+  const y = useMemo<number>(() => {
+    return note.midi / -10 + 12.7
+  }, [note.midi])
+
   return (
     <rect
       x={note.ticks / 1000}
-      y={note.midi / 10}
+      y={y}
       width={note.durationTicks / 1000}
       height=".5"
     />
