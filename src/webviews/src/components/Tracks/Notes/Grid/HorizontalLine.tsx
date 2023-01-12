32c7b8c47375b88f50a0f5ad7a4cbd9a8cfaf6f5
Isaac Poole <isaac@switcherstudio.com>
2023-01-11 22:11:24 -0500

    adds vertical lines

--- src/webviews/src/components/Tracks/Notes/Grid/HorizontalLine.tsx
+++ src/webviews/src/components/Tracks/Notes/Grid/HorizontalLine.tsx
@@ -4,29 +4,23 @@ import { Range } from '../../../classes/Range'
 export interface HorizontalLineProps {
   index: number
   range: Range
-  duration: number
   boxWidth: number
   boxHeight: number
 }
 
-export interface Coordinates {
-  x: number
-  y: number
-}
-
 export const HorizontalLine: React.FC<HorizontalLineProps> = ({
+  index,
   range,
-  duration,
   boxWidth,
   boxHeight,
 }) => {
-  const start = useMemo<Coordinates>(() => {
-    return { x: 0, y: 6 }
-  }, [])
+  const y = useMemo<number>(() => {
+    return index * (boxHeight / range.distance)
+  }, [index, boxHeight, range])
 
-  const end = useMemo<Coordinates>(() => {
-    return { x: boxWidth, y: 6 }
-  }, [])
+  const width = useMemo<number>(() => {
+    return boxWidth
+  }, [boxWidth])
 
-  return <line x1={start.x} y1={start.y} x2={end.x} y2={end.y} stroke="black" />
+  return <rect x={0} y={y} width={width} height={0.01} />
 }
--- src/webviews/src/components/Tracks/Notes/Grid/index.tsx
+++ src/webviews/src/components/Tracks/Notes/Grid/index.tsx
@@ -1,13 +1,15 @@
-import { Note as INote } from '@tonejs/midi/dist/Note'
 import React, { useMemo } from 'react'
+import * as Tone from 'tone'
 import { Range } from '../../../classes/Range'
 import { HorizontalLine } from './HorizontalLine'
+import { VerticalLine } from './VerticalLine'
 
 export interface GridProps {
   range: Range
   duration: number
   boxWidth: number
   boxHeight: number
+  disabled?: boolean
 }
 
 export const Grid: React.FC<GridProps> = ({
@@ -15,14 +17,32 @@ export const Grid: React.FC<GridProps> = ({
   duration,
   boxWidth,
   boxHeight,
+  disabled = false,
 }) => {
+  const divisions = useMemo<number>(
+    () => Math.floor(duration / Tone.Transport.PPQ),
+    [duration]
+  )
+
+  if (disabled) {
+    return null
+  }
+
   return (
     <>
       {Array.from(Array(range.distance).keys()).map(i => (
         <HorizontalLine
           index={i}
           range={range}
-          duration={duration}
+          boxWidth={boxWidth}
+          boxHeight={boxHeight}
+        />
+      ))}
+      {Array.from(Array(divisions).keys()).map(i => (
+        <VerticalLine
+          index={i}
+          range={range}
+          divisions={divisions}
           boxWidth={boxWidth}
           boxHeight={boxHeight}
         />
