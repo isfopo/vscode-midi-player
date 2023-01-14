e1789522e66684f1b8364419c9dc83981aa2c4ef
Isaac Poole <isaac@switcherstudio.com>
2023-01-14 10:38:17 -0500

    adds hook to handle zoom and offset

--- src/webviews/src/components/Tracks/index.tsx
+++ src/webviews/src/components/Tracks/index.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useMemo, useState } from 'react'
 import { Track as ITrack } from '@tonejs/midi'
 import { Track } from './Track'
 import styles from './index.module.css'
+import { usePointInRange } from '../../hooks/usePointInRange'
 
 export interface TracksProps {
   tracks: ITrack[]
@@ -9,6 +10,17 @@ export interface TracksProps {
 
 export const Tracks: React.FC<TracksProps> = ({ tracks }) => {
   const [expandedTrack, setExpandedTrack] = useState<number>(-1)
+  const {
+    point: offset,
+    increase: increaseOffset,
+    decrease: decreaseOffset,
+  } = usePointInRange(128)
+
+  const {
+    point: zoom,
+    increase: increaseZoom,
+    decrease: decreaseZoom,
+  } = usePointInRange(128, { min: 1, initial: 1 })
 
   const onClick = useCallback((index: number) => {
     setExpandedTrack(index)
---
+++ src/webviews/src/hooks/usePointInRange.ts
@@ -0,0 +1,32 @@
+import { useCallback, useState } from 'react'
+
+export interface PointInRangeOptions {
+  min?: number
+  initial?: number
+  step?: number
+}
+
+export const usePointInRange = (
+  max: number,
+  { min = 0, initial = min, step = 1 }: PointInRangeOptions = {}
+) => {
+  const [point, _setPoint] = useState<number>(initial)
+
+  const increase = useCallback(() => {
+    if (point + step < max) {
+      _setPoint(point + step)
+    }
+  }, [point, step, max])
+
+  const decrease = useCallback(() => {
+    if (point - step < min) {
+      _setPoint(point - step)
+    }
+  }, [point, step, min])
+
+  return {
+    point,
+    increase,
+    decrease,
+  }
+}
