4623e95737bee398968c62b5d35f8170d3788338
Isaac Poole <isaac@switcherstudio.com>
2023-01-15 01:13:01 -0500

    adds size option

--- src/webviews/src/components/buttons/ButtonBase/index.tsx
+++ src/webviews/src/components/buttons/ButtonBase/index.tsx
@@ -4,11 +4,16 @@ import styles from './index.module.css'
 export interface ButtonBaseProps {
   icon: any
   onClick: () => void
+  size?: 'small' | 'large'
 }
 
-export const ButtonBase: React.FC<ButtonBaseProps> = ({ icon, onClick }) => {
+export const ButtonBase: React.FC<ButtonBaseProps> = ({
+  icon,
+  onClick,
+  size,
+}) => {
   return (
-    <div className={styles['container']} onClick={onClick}>
+    <div className={`${styles['container']} ${styles[size]}`} onClick={onClick}>
       <span>{icon}</span>
     </div>
   )
