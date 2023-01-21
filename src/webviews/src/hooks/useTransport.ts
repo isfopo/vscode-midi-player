9ee66545049791c1594a4397424a1a6125c22f3f
Isaac Poole <isaac@switcherstudio.com>
2023-01-21 11:19:49 -0500

    abstracts sampler init by instrument

--- src/webviews/src/hooks/useTransport.ts
+++ src/webviews/src/hooks/useTransport.ts
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { useRef } from 'react'
 import { useCallback } from 'react'
 import * as Tone from 'tone'
+import { Piano } from '../instruments/Piano'
 
 export const useTransport = (midi: Midi) => {
   const isSetup = useRef<boolean>(false)
@@ -37,51 +38,15 @@ export const useTransport = (midi: Midi) => {
 
     //************** Create Synths and Parts, one for each track  ********************
     for (let i = 0; i < midi.tracks.length; i++) {
-      synths[i] = new Tone.Sampler({
-        urls: {
-          A0: 'A0.mp3',
-          C1: 'C1.mp3',
-          'D#1': 'Ds1.mp3',
-          'F#1': 'Fs1.mp3',
-          A1: 'A1.mp3',
-          C2: 'C2.mp3',
-          'D#2': 'Ds2.mp3',
-          'F#2': 'Fs2.mp3',
-          A2: 'A2.mp3',
-          C3: 'C3.mp3',
-          'D#3': 'Ds3.mp3',
-          'F#3': 'Fs3.mp3',
-          A3: 'A3.mp3',
-          C4: 'C4.mp3',
-          'D#4': 'Ds4.mp3',
-          'F#4': 'Fs4.mp3',
-          A4: 'A4.mp3',
-          C5: 'C5.mp3',
-          'D#5': 'Ds5.mp3',
-          'F#5': 'Fs5.mp3',
-          A5: 'A5.mp3',
-          C6: 'C6.mp3',
-          'D#6': 'Ds6.mp3',
-          'F#6': 'Fs6.mp3',
-          A6: 'A6.mp3',
-          C7: 'C7.mp3',
-          'D#7': 'Ds7.mp3',
-          'F#7': 'Fs7.mp3',
-          A7: 'A7.mp3',
-          C8: 'C8.mp3',
-        },
-        release: 1,
-        baseUrl: 'https://tonejs.github.io/audio/salamander/',
-        onload: () => {
-          new Tone.Part((time, value) => {
-            synths[i].triggerAttackRelease(
-              value.name,
-              value.duration,
-              time,
-              value.velocity
-            )
-          }, midi.tracks[i].notes).start()
-        },
+      synths[i] = new Piano(() => {
+        new Tone.Part((time, value) => {
+          synths[i].triggerAttackRelease(
+            value.name,
+            value.duration,
+            time,
+            value.velocity
+          )
+        }, midi.tracks[i].notes).start()
       }).toDestination()
     }
   }, [midi])
